e34fad9b242a233149648aea617408b519a0de0c
Ameer Al Issa <ameerwisam97@gmail.com>
2021-03-10 12:06:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SortAlgorthim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var count = Convert.ToInt32(Console.ReadLine());
+            var Orginal = new int[] { 81,22,16,8,12,67,88,19,99 };
+            var count = Orginal.Length;
+            Console.WriteLine(string.Join(' ', Orginal));
+
+            int temp;
+            for (int i = 0; i < count -1; i++)
+            {
+                if(Orginal[i] > Orginal[i+1])
+                {
+                    temp = Orginal[i];
+                    Orginal[i] = Orginal[i + 1];
+                    Orginal[i + 1] = temp;
+                    i = -1;
+                }
+            }
+            Console.WriteLine(string.Join(' ', Orginal));
+        }
+    }
+}
