27f804516f01dd9af25d27d87ddb914ffa2beb15
Ameer Al Issa <ameerwisam97@gmail.com>
2021-03-10 12:08:04 +0300

    fix name

--- Program.cs
+++ Program.cs
@@ -7,22 +7,22 @@ namespace SortAlgorthim
         static void Main(string[] args)
         {
             //var count = Convert.ToInt32(Console.ReadLine());
-            var Orginal = new int[] { 81,22,16,8,12,67,88,19,99 };
-            var count = Orginal.Length;
-            Console.WriteLine(string.Join(' ', Orginal));
+            var Arr = new int[] { 81,22,16,8,12,67,88,19,99 };
+            var count = Arr.Length;
+            Console.WriteLine(string.Join(' ', Arr));
 
             int temp;
             for (int i = 0; i < count -1; i++)
             {
-                if(Orginal[i] > Orginal[i+1])
+                if(Arr[i] > Arr[i+1])
                 {
-                    temp = Orginal[i];
-                    Orginal[i] = Orginal[i + 1];
-                    Orginal[i + 1] = temp;
+                    temp = Arr[i];
+                    Arr[i] = Arr[i + 1];
+                    Arr[i + 1] = temp;
                     i = -1;
                 }
             }
-            Console.WriteLine(string.Join(' ', Orginal));
+            Console.WriteLine(string.Join(' ', Arr));
         }
     }
 }
